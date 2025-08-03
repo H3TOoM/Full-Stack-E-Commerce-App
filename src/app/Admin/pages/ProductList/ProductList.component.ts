fe76e996b952cc9dae4100966a9277c798fc60c6
Hatim <altorbed7@gmail.com>
2025-08-03 15:02:45 +0300

    build product list

--- src/app/Admin/pages/ProductList/ProductList.component.ts
+++ src/app/Admin/pages/ProductList/ProductList.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../services/Product/Product.service';
+import { Product } from '../../../models/Product';
+import { CategoryService } from '../../../services/Category/Category.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ProductList',
   templateUrl: './ProductList.component.html',
-  styleUrls: ['./ProductList.component.css']
+  styleUrls: ['./ProductList.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private _ProductService: ProductService,
+    private _CategoryService: CategoryService
+  ) {}
 
   ngOnInit() {
+    this.getProducts();
   }
 
+  products!: Product[];
+
+  getProducts() {
+    this._ProductService.getProducts().subscribe({
+      next: (res: Product[]) => {
+        this.products = res;
+        console.log(this.products);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  getCategory(id: number) {
+    this._CategoryService.getCategoryById(id).subscribe({
+      next: (res) => {
+        return res;
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  deleteProduct(id: number) {
+    this._ProductService.deleteProduct(id).subscribe({
+      next: (res) => {
+        Swal.fire({
+          title: 'Product Deleted Successfully!',
+          icon: 'success',
+          showConfirmButton: false,
+        });
+
+        console.log(res);
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
